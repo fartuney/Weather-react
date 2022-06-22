e6c29077e140d34beee76bb57f7d8ee7db24c107
fartuney <102562065+fartuney@users.noreply.github.com>
2022-06-22 15:41:48 -0600

    removed exclamation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>👩‍🦰Fartuney!!!!</h1>
+        <h1>👩‍🦰Fartuney!!!</h1>
         <Weather city="Edmonton" />
       </header>
     </div>
